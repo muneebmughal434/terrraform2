86d57d9585992ba3698655d6cfbc7da44746df6c
Muhammad Muneeb <85062890+muneebmughal434@users.noreply.github.com>
2025-08-12 21:26:26 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region   # if you don't have variables.tf, change to region = "us-east-1"
 }
 
-# Generate a unique bucket suffix so names don't collide globally
+# Unique bucket name each run
 resource "random_id" "suffix" {
   byte_length = 4
 }
@@ -19,16 +19,13 @@ locals {
   bucket_name = "tf-site-${random_id.suffix.hex}"
 }
 
-# S3 bucket
+# Bucket
 resource "aws_s3_bucket" "site" {
   bucket = local.bucket_name
-
-  tags = {
-    project = "lab3-terraform-github-actions"
-  }
+  tags = { project = "lab3-terraform-github-actions" }
 }
 
-# Allow public website hosting (for lab/testing only)
+# Turn OFF bucket-level public blocks so ACLs work (lab only)
 resource "aws_s3_bucket_public_access_block" "site" {
   bucket                  = aws_s3_bucket.site.id
   block_public_acls       = false
@@ -37,54 +34,45 @@ resource "aws_s3_bucket_public_access_block" "site" {
   restrict_public_buckets = false
 }
 
-# Website configuration
+# Website hosting
 resource "aws_s3_bucket_website_configuration" "site" {
   bucket = aws_s3_bucket.site.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "index.html"
-  }
-
+  index_document { suffix = "index.html" }
+  error_document { key    = "index.html" }
   depends_on = [aws_s3_bucket_public_access_block.site]
 }
 
-# Public read policy for website objects (testing only)
-data "aws_iam_policy_document" "public_read" {
-  statement {
-    sid     = "PublicReadGetObject"
-    effect  = "Allow"
-    actions = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.site.arn}/*"]
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "public_read" {
+# Enable object ACLs
+resource "aws_s3_bucket_ownership_controls" "site" {
   bucket = aws_s3_bucket.site.id
-  policy = data.aws_iam_policy_document.public_read.json
+  rule { object_ownership = "ObjectWriter" }
 }
 
-# Upload the page
+# Make the bucket public via ACL (lab only)
+resource "aws_s3_bucket_acl" "site" {
+  bucket = aws_s3_bucket.site.id
+  acl    = "public-read"
+  depends_on = [aws_s3_bucket_ownership_controls.site]
+}
+
+# Upload the page (object itself public)
 resource "aws_s3_object" "index" {
   bucket       = aws_s3_bucket.site.id
   key          = "index.html"
   source       = "${path.module}/index.html"
   content_type = "text/html"
   etag         = filemd5("${path.module}/index.html")
+  acl          = "public-read"
+  depends_on   = [aws_s3_bucket_acl.site]
 }
 
+# Outputs
 output "bucket_name" {
-  value = aws_s3_bucket.site.bucket
+  value       = aws_s3_bucket.site.bucket
+  description = "Name of the S3 bucket"
 }
 
 output "website_endpoint" {
-  value = aws_s3_bucket_website_configuration.site.website_endpoint
-  description = "Open this URL to view the site after apply."
+  value       = aws_s3_bucket_website_configuration.site.website_endpoint
+  description = "S3 static website URL"
 }
